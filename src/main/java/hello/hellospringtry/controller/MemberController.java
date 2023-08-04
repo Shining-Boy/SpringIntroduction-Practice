9a6f727212feb36b914c803dc561e3ee075f0fe1
hsbae <hellospring89@gmail.com>
2023-08-04 09:46:26 +0900

    회원가입 회원조회 기능 추가

--- src/main/java/hello/hellospringtry/controller/MemberController.java
+++ src/main/java/hello/hellospringtry/controller/MemberController.java
@@ -1,14 +1,45 @@
 package hello.hellospringtry.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hello.hellospringtry.domain.Member;
 import hello.hellospringtry.service.MemberService;
 
+@Controller
 public class MemberController {
     private final MemberService memberService;
     
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(Member form){
+        Member member = new Member();
+        member.setName(form.getName());
+        
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+
+        return "members/memberList";
     }
 }
