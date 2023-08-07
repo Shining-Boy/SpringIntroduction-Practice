11448cf2b9fc1c661aafd516e5a68b6629eb13d2
hsbae <hellospring89@gmail.com>
2023-08-07 11:22:01 +0900

    Jdbc 리포지토리 구현

--- src/main/java/hello/hellospringtry/SpringConfig.java
+++ src/main/java/hello/hellospringtry/SpringConfig.java
@@ -1,14 +1,23 @@
 package hello.hellospringtry;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import hello.hellospringtry.repository.JdbcMemberRepository;
 import hello.hellospringtry.repository.MemberRepository;
 import hello.hellospringtry.repository.MemoryMemberRepository;
 import hello.hellospringtry.service.MemberService;
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     MemberService memberService() {
@@ -17,6 +26,7 @@ public class SpringConfig {
     
     @Bean
     MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
